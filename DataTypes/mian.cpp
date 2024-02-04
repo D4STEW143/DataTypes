f7f1c098f6ace682c80190979c64b352d327ee01
Lapin_qh81@top-academy.ru <Lapin_qh81@top-academy.ru>
2024-02-04 10:20:04 +0900

    For study purposes only.

---
+++ DataTypes/mian.cpp
@@ -0,0 +1,99 @@
+﻿#include<iostream>
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+
+//#define LOGICAL_TYPES
+//#define NUMERIC_TYPES
+//#define VARIABLES
+//#define CONSTANTS
+
+void main()
+
+{
+	setlocale(LC_ALL, "Russian");
+#if defined LOGICAL_TYPES
+	cout << "DataTypes:" << endl;
+	cout << endl;
+	cout << true << endl;
+	cout << false << endl;
+	cout << endl;
+#endif
+
+#if defined NUMERIC_TYPES
+	cout << "\n-----------------------------------\n";
+	cout << "int:\n";
+	cout << sizeof(int) << endl;
+	cout << INT_MIN << "..." << INT_MAX << endl;
+	cout << "\n-----------------------------------\n";
+
+	cout << endl;
+
+	cout << "\n-----------------------------------\n";
+	cout << "long:\n";
+	cout << sizeof(long) << endl;
+	cout << LONG_MIN << "..." << LONG_MAX << endl;
+	cout << "\n-----------------------------------\n";
+
+	cout << endl;
+
+	cout << "\n-----------------------------------\n";
+	cout << "long long:\n";
+	cout << sizeof(long long) << endl;
+	cout << "Unsigned Long long:" << "0" << "..." << ULLONG_MAX << endl;
+	cout << "Signed Long long:" << LLONG_MIN << "..." << LLONG_MAX << endl;
+	cout << "\n-----------------------------------\n";
+
+	cout << endl;
+
+	cout << "\n-----------------------------------\n";
+	cout << "float:\n";
+	cout << sizeof(float) << endl;
+	cout << FLT_MIN << "..." << FLT_MAX << endl;
+	cout << "\n-----------------------------------\n";
+
+	cout << endl;
+
+	cout << "\n-----------------------------------\n";
+	cout << "double:\n";
+	cout << sizeof(double) << endl;
+	cout << DBL_MIN << "..." << DBL_MAX << endl;
+	cout << "\n-----------------------------------\n";
+
+#endif
+
+#if defined VARIABLES
+	int a = 0;
+	cout << a << endl;
+	double price; // объявление переменной
+	price = 0; //инициализация после объявления. 
+	cout << price << endl;
+	double price_of_coffee;
+	cout << "Введите стоимость кофе:" << endl;
+	cin >> price_of_coffee; //инициализация вводом с клавиатуры.
+	int number_of_cups;
+	cout << "Введите количество чашек" << endl; 
+	cin >> number_of_cups;
+	double total_price = price_of_coffee * number_of_cups;
+	cout << "Общая стоимость покупки: " << total_price << endl;
+#endif
+
+#ifdef CONSTANTS
+	int speed = 0;
+	const int MAX_SPEED = 250; 
+	cout << typeid(5).name() << endl;
+	cout << typeid(55.).name() << endl;
+	double test = 55.7;
+	cout << typeid(test).name() << endl;
+	55; //числовая константа типа int
+	55.; //числовая константа типа double
+	.55; //числовая константа типа double
+	.55f; //числовая константа типа float
+	55l; //long
+	55ll; //long_long
+	55ull; //unsigned long_long
+	cout << typeid(.55f).name() << endl;
+#endif
+
+}
---
+++ Operators/main.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+using namespace std;
+
+//#define ARITHMETICAL_OPERATORS 
+//#define ASSIGNMENT_OPERATORS
+//#define INCREMENT_DECREMENT
+//#define COMPAUND_ASSIGNMENTS
+void main()
+{
+	setlocale (LC_ALL, "");
+	cout << "Hello operators" << endl;
+#ifdef ARITHMETICAL_OPERATORS
+	//Unary +, -;
+	//Binary +, -, *, /, %;
+	//cout << 17 / 3 << endl;
+	//cout << 17 % 3 << endl;
+	cout << 3. / 17 << endl;
+	cout << 3. % 17 << endl;
+#endif
+
+#ifdef ASSIGNMENT_OPERATORS
+	int a = 2; //= это оператор присвоить. 
+	//l-value это int a, а r-value это 2.
+	int a1, b, c; //объявление по ассоциации
+	a1 = b = c = 0; //инициализация по ассоциации (справа налево)
+	cout << a << "\t" << b << "\t" << c << endl;
+#endif
+
+#ifdef INCREMENT_DECREMENT
+	int a2 = 0;
+	++a2; //префиксный инкремент
+	a2--; //постфиксный декремент
+	int j = a2++;
+	int g = ++a2;
+	cout << a2 << endl;
+	cout << j << endl;
+	cout << g << endl;
+#endif
+
+#ifdef COMPAUND_ASSIGNMENTS
+
+	int a_r, b_r;
+	a_r = 2;
+	b_r = 3;
+	a_r = a_r + b_r;
+	cout << a_r << endl;
+	a_r += b_r;
+	cout << a_r << endl;
+
+#endif
+}
+//кидаю на гитха для дз.
